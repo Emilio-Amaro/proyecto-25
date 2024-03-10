83d08260b34fcc758e075d87a172bb7129263d15
Emilio-Amaro <156143027+Emilio-Amaro@users.noreply.github.com>
2024-03-09 20:59:07 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,189 +1,87 @@
-var PLAY = 1;
-var END = 0;
-var gameState = PLAY;
-var bow , arrow,  background, redB, pinkB, greenB ,blueB ,arrowGroup;
-var bowImage, arrowImage, green_balloonImage, red_balloonImage, pink_balloonImage ,blue_balloonImage, backgroundImage;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
-var score =0;
+var ball,groundObj,leftSide,rightSide;
+var world;
+var radius = 70;
+
 function preload(){
-  
-  backgroundImage = loadImage("background0.png");
-  
-  arrowImage = loadImage("arrow0.png");
-  bowImage = loadImage("bow0.png");
-  red_balloonImage = loadImage("red_balloon0.png");
-  green_balloonImage = loadImage("green_balloon0.png");
-  pink_balloonImage = loadImage("pink_balloon0.png");
-  blue_balloonImage = loadImage("blue_balloon0.png");
-  
-}
 
+	dustbinImg = loadImage("dustbin.png");
+	paperImg = loadImage("paper.png");
+
+	//dustbinImg = addImage("dustbin.png");
+	//paperImg = addImage("paper.png");
+
+	//dustbin.loadImage("dustbin.png");
+	//paper.addImage("paper.png");
+	
+	//dustbin.loadImage("dustbin.png");
+	//paper.loadImage("paper.png");
+}
 
 
 function setup() {
-  createCanvas(400, 400);
-  
-  //crear fondo
-  scene = createSprite(0,0,400,400);
-  scene.addImage(backgroundImage);
-  scene.scale = 2.5
-  
-  //crear el arco para disparar las flechas
-  bow = createSprite(380,220,20,50);
-  bow.addImage(bowImage); 
-  bow.scale = 1;
-  
-   score = 0  
-  redB= new Group();
-  greenB= new Group();
-  blueB= new Group();
-  pinkB= new Group();
-  arrowGroup= new Group();
- 
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	var ball_options={
+		isStatic:false,
+		restitution:0.3,
+		density:0.4
+	}
+
+	ball = Bodies.circle(260,100,radius/2.6,ball_options);
+	World.add(world,ball);
+
+	ground=new Ground(width/2,670,width,20);
+	leftSide = new Ground(1100,600,10,120);
+	rightSide = new Ground(1270,600,10,120);
+	bottomSide = new Ground(1185,650,150,20);
+
+	Engine.run(engine);
   
 }
+
 
 function draw() {
- background(0);
- if(gameState === PLAY){
+	background(200);
+	rectMode(CENTER);
 
-  //mover suelo
-    scene.velocityX = -3 
 
-    if (scene.x < 0){
-      scene.x = scene.width/2;
-    }
-  
-  //mover el arco
-  bow.y = World.mouseY
-  
-   //liberar la flecha al presionar la barra espaciadora
-  if (keyDown("space")) {
-    createArrow();
+	ground.display();
+	leftSide.display();  
+	rightSide.display();
+	bottomSide.display();
+
+	
+	imageMode(CENTER);
+
+	//image(paperImg,ball.position.y,ball.position.x,radius,radius);//comentar
+	//image(paperImg,ball.position.x,ball.position.y,radius/2,radius/2);//comentar
+	//ellipse(ball.position.x,ball.position.y,radius,radius);//comentar
+	image(paperImg,ball.position.x,ball.position.y,radius,radius);//comentar
+
+
+	
+	//image(1185, 570, 200,200);//comentar
+	//rect(1185, 570, 200,200);//comentar
+	image(dustbinImg, 1185, 570, 200,200);//commentar
+	//ellipse(1185, 570, 200,200)//comentar
+
+
+
+}
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+		Matter.Body.applyForce(ball,ball.position,{x:85,y:-85});
     
-  }
-  
-  //crear enemigos continuos
-  var select_balloon = Math.round(random(1,4));
-  
-  if (World.frameCount % 100 == 0) {
-    switch(select_balloon ){
-      case 1: redBalloon();
-      break;
-      case 2:blueBalloon();
-      break;
-      case 3:pinkBalloon();
-      break;
-      case 4:greenBalloon();
-      break;
-      default:break;
-    }
-  }
-
-  /*Descomentar la sentencia correcta para que 
-  el juego cambie a estado "END" 
-  al golpear el globo rojo*/
-  if (arrowGroup.isTouching(redB)) 
-  //if (arrowGroup.Collide(redB)) 
-  //if (arrowGroup.isCollide(redB)) 
-  //if(frameCount>700)//comentar esta línea después de seleccionar la solución
-  {
-    redB.destroyEach();
-    gameState=END; 
-   }
- 
-  if (gameState === END) {
-  bow.destroy();
-  scene.velocityX = 0;
-}
-
-
- if (arrowGroup.isTouching(greenB)) {
-  greenB.destroyEach();
-  arrowGroup.destroyEach();
-  score=score+3;
-}
-
-/*Descomentar el el bloque de código if correcto para 
-destruir el globo azul al ser  
-golpeado por las flechas */
-
-  if (arrowGroup.isTouching(blueB)) {
- blueB.destroyEach();
-  arrowGroup.destroyEach();
-   score=score+2;
- }
-
-
-//  if (arrowGroup.isTouching(redB)) {
-//   blueB.destroyEach();
-//   arrowGroup.destroyEach();
-// }
-
-
-//  if (arrowGroup.isTouching(blueB)) {
-//   arrowGroup.destroyEach();
-// }
-
-
-if (arrowGroup.isTouching(pinkB)) {
-  pinkB.destroyEach();
-  arrowGroup.destroyEach();
-  score=score+1;
-}
- }
-  
-  drawSprites();
-  text("Puntuación: "+ score, 300,50);
-}
-
-
-function redBalloon() {
-  var red = createSprite(0,Math.round(random(20, 370)), 10, 10);
-  red.addImage(red_balloonImage);
-  red.velocityX = 3;
-  red.lifetime = 150;
-  red.scale = 0.1;
-  redB.add(red);
-}
-
-function blueBalloon() {
-  var blue = createSprite(0,Math.round(random(20, 370)), 10, 10);
-  blue.addImage(blue_balloonImage);
-  blue.velocityX = 3;
-  blue.lifetime = 150;
-  blue.scale = 0.1;
-  blueB.add(blue);
-}
-
-function greenBalloon() {
-  var green = createSprite(0,Math.round(random(20, 370)), 10, 10);
-  green.addImage(green_balloonImage);
-  green.velocityX = 3;
-  green.lifetime = 150;
-  green.scale = 0.1;
-  greenB.add(green);
-}
-
-function pinkBalloon() {
-  var pink = createSprite(0,Math.round(random(20, 370)), 10, 10);
-  pink.addImage(pink_balloonImage);
-  pink.velocityX = 3;
-  pink.lifetime = 150;
-  pink.scale = 1
-  pinkB.add(pink);
-}
-
-
-//crear las flechas para el arco
- function createArrow() {
-  var arrow= createSprite(100, 100, 60, 10);
-  arrow.addImage(arrowImage);
-  arrow.x = 360;
-  arrow.y=bow.y;
-  arrow.velocityX = -4;
-  arrow.lifetime = 100;
-  arrow.scale = 0.3;
-  arrowGroup.add(arrow);
-   
+  	}
 }
